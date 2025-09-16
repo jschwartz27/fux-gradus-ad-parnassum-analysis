3b5b37971302f4643352556583a835a1a774d8f4
jschwartz27 <josh.schwartz92@gmail.com>
2025-09-16 22:19:13 +0200

    implement fux-enums

--- logic/models/entries.py
+++ logic/models/entries.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from logic.models.enums.entry_type import EntryType
 from logic.models.enums.loci_type import LociType
 from logic.models.enums.relation_type import RelationType
+from logic.models.enums.fux import NVoices, Species
 import uuid
 
 # Rule: boolean
@@ -12,7 +13,10 @@ import uuid
 @dataclass(frozen=True)
 class Entry:
     Guid: uuid.UUID
+    NVoice: NVoices
+    Species: Species
     Text: str
+    AuthorsNote: str
     Note: str
     PageNumberFrom: int
     PageNumberTo: int
---
+++ logic/models/enums/fux.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Species(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+
+
+class NVoices(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
