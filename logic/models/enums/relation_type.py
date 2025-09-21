e8f965ff1a3ffb5d06138d0cb3b388bc54a028d9
jschwartz27 <josh.schwartz92@gmail.com>
2025-09-21 21:40:55 +0200

    updates

--- logic/models/enums/relation_type.py
+++ logic/models/enums/relation_type.py
@@ -5,3 +5,14 @@ class RelationType(IntEnum):
     INTRA_VOICE_MOVEMENT = 0
     INTER_VOICE_MOVEMENT = 1
     CONFIG = 2
+
+
+class InterVoiceMovementTypes(IntEnum):
+    DEFAULT = 0  # both intra and inter
+    INTRA_MEASURE = 1
+    INTER_MEASURE = 2
+
+
+class ConfigTypes(IntEnum):
+    CONFIG_LOCI = 0
+    CONFIG_RELAIIONS = 1
